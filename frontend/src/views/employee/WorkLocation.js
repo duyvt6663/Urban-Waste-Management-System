5bb071b2c78f65a08e065de71fb502a952406a71
duyvt6663 <93929554+duyvt6663@users.noreply.github.com>
2022-12-05 22:06:04 +0700

    minor update

---
+++ frontend/src/views/employee/WorkLocation.js
@@ -0,0 +1,150 @@
+// eslint-disable prettier/prettie 
+import React, { useEffect, useState } from 'react'
+import { Routes, Route, useParams } from 'react-router-dom'
+import useAxiosPrivate from 'src/hooks/useAxiosPrivate'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import Routing from '../AppMap/RoutingMachine';
+import { iconDEPOT, iconTREAT, iconMCP } from '../AppMap/RoutingMachine'
+import { checkIsDepot, checkIsTreat } from 'src/utils/utils';
+import {
+    CContainer,
+    CRow,
+    CCol,
+    CButton,
+    CButtonGroup,
+    CCallout,
+    CListGroup,
+    CListGroupItem,
+} from '@coreui/react'
+import { response } from 'express';
+
+const EMPLOYEE = '/employee/'
+const MCP_LIST = '/map/'
+const ROUTE_LIST = '/map/route/'
+const position = [10.7724483, 106.6582936]
+const maps = {
+    base: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+};
+
+const WorkLocation = () => {
+    const axiosPrivate = useAxiosPrivate()
+    const params = useParams()
+    const [profile, setProfile] = useState(null)
+    const [Markers, setMarkers] = useState(null)
+    const [MCPs, setMCPs] = useState([])
+    const [Routes, setRoutes] = useState([])
+    const [map, setMap] = useState(null)
+    const [route, setRoute] = useState(null)
+
+    useEffect(() => {
+        const getProfile = async () => {
+            try {
+                const response = await axiosPrivate.get(`${EMPLOYEE}${params.id}/`);
+                // console.log("Employee -> ", response.data)
+                if (response) {
+                    setProfile(response.data)
+                }
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        const getMCPs = async () => {
+            try {
+                const response = await axiosPrivate.get(MCP_LIST);
+                setMCPs(response.data[0])
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        const getRoutes = async () => {
+            try {
+                const response = await axiosPrivate.get(ROUTE_LIST);
+                setRoutes(response.data)
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        const getRoute = async () => {
+            try {
+                const response = await axiosPrivate.get(`${ROUTE_LIST}${profile.route_id}/`)
+                setRoute(response.data)
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        getProfile()
+        getMCPs()
+        getRoutes()
+        getRoute()
+        handleCurrentRoute()
+    }, [])
+
+    function handleAssign() {
+        // pop route table on
+
+    }
+    async function handleCurrentRoute() {
+        // display current route layout
+        setMarkers(() => {
+            // filter out which MCPs is in current route
+            let res = []
+            for (let i = 0; i < profile.ordered_MCPs.length; ++i)
+                res.push(MCPs.filter((mcp) => { return mcp.asset_id == profile.ordered_MCPs[i] })[0])
+            return <Routing routeInfo={res} route={Routes.filter((route => { return route.id == profile.route_id }))[0]} key={profile.route_id} />
+        })
+    }
+
+
+    return (
+        <>
+            <CContainer fluid>
+                <CRow>
+                    <CCol lg={9}>
+                        <MapContainer center={position} zoom={16} whenReady={(map) => setMap(map)}>
+                            <TileLayer
+                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                url={maps.base}
+                            />
+                            {Markers}
+                        </MapContainer>
+                    </CCol>
+
+                    <CCol>
+                        <CButtonGroup>
+                            <CCol lg={1.2}>
+                                <CButton color="primary" onClick={handleCurrentRoute} size="sm">
+                                    Current route
+                                </CButton>
+                            </CCol>
+                            <CButton color="primary" onClick={handleAssign} size="sm">
+                                Assign
+                            </CButton>
+                            <CButton color="primary" onClick={handleAssign} size="sm">
+                                Optimize
+                            </CButton>
+                        </CButtonGroup>
+                    </CCol>
+                </CRow>
+                <CRow>
+                    <CListGroup>
+                        {/* {Routes.map(x => (
+                            <CButton key={x.id} size="sm" variant='outline' color='danger' onClick={() => { handleClickRoute(x.id) }}>
+                                <CRow>
+                                    <CCol>
+                                        route {x.id}
+                                    </CCol>
+                                    <CCol>
+                                        <CButton color="primary" onClick={(e) => { e.stopPropagation(); handleDelete(x.id) }} size="sm">
+                                            Delete
+                                        </CButton>
+                                    </CCol>
+                                </CRow>
+                            </CButton>
+                        ))} */}
+                    </CListGroup>
+                </CRow>
+            </CContainer >
+        </>
+    )
+}
+export default WorkLocation
